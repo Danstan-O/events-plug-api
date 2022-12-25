a58739568b6d3a26c85fab3011532dd65fb33e4c
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-25 07:39:42 +0300

    enable private method to permit user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
   def create
     user = User.create(user_params)
     if user.valid?
@@ -12,6 +13,7 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
   def show
     user = User.find_by(id: session[:user_id])
     if user
@@ -19,5 +21,11 @@ class UsersController < ApplicationController
     else
       render json: { error: "Not authorized" }, status: :unauthorized
     end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :role, :password, :password_confirmation)
   end
 end

eedf04f9d1c4c5d94986a3ce48e5381bd01f4f15
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-25 07:39:08 +0300

    login user -> registration & login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,12 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
 end

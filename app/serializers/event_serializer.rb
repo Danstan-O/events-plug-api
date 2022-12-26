bae8f3f4c6560ef00a19710ac46ce1fd9954629d
danstan <danstan.oduor@student.moringaschool.com>
2022-12-26 15:32:11 +0300

    edited EventSerializer to include price and image

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,3 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :location, :category, :description, :like, :start_date, :end_date, :start_time, :end_time
+  attributes :id, :name, :address, :location, :category, :description, :like, :price, :image, :start_date, :end_date, :start_time, :end_time
 end

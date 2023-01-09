8dabf9f8839cc15549d98e49f750ed638b479622
danstan <danstan.oduor@student.moringaschool.com>
2023-01-09 14:04:08 +0300

    added a create endpoint for checkout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,2 +1,14 @@
 class CheckoutsController < ApplicationController
+    def create
+        checkout =  Checkout.create!(checkout_params)
+        # session[:user_id] = user.id
+
+        render json: checkout, status: :created
+    end
+
+    private
+    def checkout_params
+        params.permit(:first_name, :last_name, :email, :card_info, :expiry_date, :cvc)
+            # , :address_2, :city, :county)
+    end
 end

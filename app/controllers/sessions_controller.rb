a3f6cd011caf193d5132af429e5c0835e7d6cc1c
brayarn <87235646+o-brayarn@users.noreply.github.com>
2023-01-09 17:27:51 +0300

    Ft feature (#5)
    * display all available users
    
    * create user via registration
    
    * login user -> registration & login
    
    * enable private method to permit user creation
    
    * ft-feature (#1)
    
    * display all available users
    
    * create user via registration
    
    * login user -> registration & login
    
    * enable private method to permit user creation
    
    * seeded a few event data to check out it works
    
    * added an index and show controllers
    
    * added a byebug gem to the gemfile
    
    * added a byebug gem
    
    * byebug gem got installed
    
    * made a byebug change
    
    * Added a migration file for image
    
    * Added a migration file for price
    
    * edited my seeds to include price and image
    
    * edited EventSerializer to include price and image
    
    * edited event route to have a show and index routes
    
    * price and image migration were added to the schema
    
    * add devise for smtp
    
    * create user session
    
    * destroy session
    
    * add cookies
    
    * validate user information
    
    * install & implement  devise
    
    * setup devise
    
    * set abstract class
    
    * Danstan (#4)
    
    * seeded a few event data to check out it works
    
    * added an index and show controllers
    
    * added a byebug gem to the gemfile
    
    * added a byebug gem
    
    * byebug gem got installed
    
    * made a byebug change
    
    * Added a migration file for image
    
    * Added a migration file for price
    
    * edited my seeds to include price and image
    
    * edited EventSerializer to include price and image
    
    * edited event route to have a show and index routes
    
    * price and image migration were added to the schema
    
    * removed the created-at and updated-at from the events controller
    
    Co-authored-by: danstan <danstan.oduor@student.moringaschool.com>
    Co-authored-by: Danstan-O <110296805+Danstan-O@users.noreply.github.com>

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  before_action :authorize
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { error: "Invalid email or password" }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
+
+  private
+
+  def authorize
+    return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :user_events
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :role, presence: true
 end

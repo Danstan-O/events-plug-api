21696ea3c9c77962a8d3f6a41b686b296f39f355
danstan <danstan.oduor@student.moringaschool.com>
2023-01-09 17:58:59 +0300

    updated the accounts information to include a create endpoint

--- app/controllers/account_infos_controller.rb
+++ app/controllers/account_infos_controller.rb
@@ -1,2 +1,14 @@
 class AccountInfosController < ApplicationController
+    def create
+        account_infos =  AccountInfo.create!(account_infos_params)
+        # session[:user_id] = user.id
+
+        render json: account_infos, status: :created
+    end
+
+    private
+    def account_infos_params
+        params.permit(:first_name, :last_name, :email, :phone_number, :billing_address)
+            # , :address_2, :city, :county)
+    end
 end

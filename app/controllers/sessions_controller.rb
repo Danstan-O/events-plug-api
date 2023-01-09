ce7086fbf803921fa48d799ef3a12569ea5706aa
Brayarn <brayarn.oduor@student.moringaschool.com>
2023-01-09 16:05:52 +0300

    create user session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { error: "Invalid email or password" }, status: :unauthorized
+    end
+  end

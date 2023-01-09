e094754c6ffdf439d1d19a3ca68ec5488fd04d7a
danstan <danstan.oduor@student.moringaschool.com>
2023-01-09 16:51:13 +0300

    added a create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :account_infos
   resources :checkouts
   resources :user_events
-  resources :events, only: [:show, :index]
+  resources :events, only: [:show, :index, :create]
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

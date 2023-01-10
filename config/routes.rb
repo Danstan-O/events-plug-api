b33f25787407495d1bc829eca988cc26e0f9c59d
Danstan-O <110296805+Danstan-O@users.noreply.github.com>
2023-01-10 08:57:09 +0300

    updated branch danstan  (#9)
    * added custom routes
    
    * commented out the before_action
    
    * configured my code to use sessions and cookies

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
 
   resources :users
+  post "/register", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

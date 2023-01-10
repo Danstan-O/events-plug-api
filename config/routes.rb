c78176a722ea3aec829731fc83d94b29b81f10f8
danstan <danstan.oduor@student.moringaschool.com>
2023-01-10 08:42:31 +0300

    added custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :user_events
   resources :events, only: [:show, :index]
   resources :users
+  post "/register", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

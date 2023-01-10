d8649487a4285e17262084dbcdc5b164b0659c21
Brayarn <brayarn.oduor@student.moringaschool.com>
2023-01-10 10:40:32 +0300

    create checkout-page

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,2 +1,25 @@
 class CheckoutsController < ApplicationController
+  def index
+    render json: Checkout.all
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user, serializer: CheckoutSerializer
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
+
+  def create
+    checkout = Checkout.create!(checkout_params)
+    render json: checkout
+  end
+
+  private
+
+  def checkout_params
+    params.permit(:first_name, :last_name, :email, :card_info, :expiry_date, :cvc)
+  end
 end

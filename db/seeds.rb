9e19d84f28d964feaa2a055f90b00c6007aa8d53
Brayarn <brayarn.oduor@student.moringaschool.com>
2023-01-10 10:44:02 +0300

    create seed  sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: Luke, movie: movies.first)
 
 event1 = Event.create!([name: "Blockchain Summit", address: "3422-00100", location: "Diamond Plaza", category: "Technology, Metaverse", description: "The Blockchain Summit 2023 recaps the events that nearly brought the crypto industry down, the potential of blockchain technology in handling real world problem including drought, and what CBDC means to crypto.", like: true, start_date: "20/01/2023", end_date: "27/01/2023", start_time: "10:00", end_time: "16:30"])
+user2 = User.create!([name: "Sammie Sam", email: "SamOrganizer@example.com", role: "Organizer", password: "organizer1234", password_confirmation: "organizer1234"])
 
 user1 = User.create!([name: "Admin", email: "admin@example.com", role: "admin", password: "admin", password_confirmation: "admin"])
 user2 = User.create!([name: "Sammie", email: "SamOrganizer@example.com", role: "organizer", password: "organizer", password_confirmation: "organizer"])

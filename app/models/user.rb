f292b941166f25243774a8daf533b5de3a413086
danstan <danstan.oduor@student.moringaschool.com>
2023-01-10 20:27:25 +0300

    added a relationship to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :user_events
+  has_many :users, through: :user_events
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true

570d06d29cd0d5d556501820a0e9648cbd73cc5e
Brayarn <brayarn.oduor@student.moringaschool.com>
2023-01-10 10:49:57 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :user_events
   resources :events
   resources :users
+  post "/register", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

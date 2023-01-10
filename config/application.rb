c1bb9a26fcc8d2d8196129ad32bb9cc91db2b80d
Brayarn <brayarn.oduor@student.moringaschool.com>
2023-01-10 10:41:43 +0300

    add middlewares

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ Bundler.require(*Rails.groups)
 
 module EventPlug
   class Application < Rails::Application
+    config.middleware.use ActionDispatch::Cookies
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 

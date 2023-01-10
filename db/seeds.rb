b6b54d3681ee295a4edd360d435a48ceb9663c6b
Brayarn <brayarn.oduor@student.moringaschool.com>
2023-01-10 10:49:24 +0300

    add sample data for seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,14 @@
 #
 #   movies = Movie.create([{ name: Star Wars }, { name: Lord of the Rings }])
 #   Character.create(name: Luke, movie: movies.first)
-
+Event.destroy_all
 event1 = Event.create!([name: "Blockchain Summit", address: "3422-00100", location: "Diamond Plaza", category: "Technology, Metaverse", description: "The Blockchain Summit 2023 recaps the events that nearly brought the crypto industry down, the potential of blockchain technology in handling real world problem including drought, and what CBDC means to crypto.", like: true, start_date: "20/01/2023", end_date: "27/01/2023", start_time: "10:00", end_time: "16:30"])
-user2 = User.create!([name: "Sammie Sam", email: "SamOrganizer@example.com", role: "Organizer", password: "organizer1234", password_confirmation: "organizer1234"])
 
-user1 = User.create!([name: "Admin", email: "admin@example.com", role: "admin", password: "admin", password_confirmation: "admin"])
-user2 = User.create!([name: "Sammie", email: "SamOrganizer@example.com", role: "organizer", password: "organizer", password_confirmation: "organizer"])
-user3 = User.create!([name: "Mary", email: "mary@example.com", role: "user", password: "user3", password_confirmation: "user3"])
+User.destroy_all
+user1 = User.create!([name: "Admin Adam", email: "admin@example.com", role: "Admin", password: "admin1234", password_confirmation: "admin1234"])
+user2 = User.create!([name: "Sammie Sam", email: "SamOrganizer@example.com", role: "Organizer", password: "organizer1234", password_confirmation: "organizer1234"])
+user3 = User.create!([name: "Mary Marian", email: "mary@example.com", role: "User", password: "user1234", password_confirmation: "user1234"])
+user4 = User.create!([name: "John Smith", email: "smithj@example.com", role: "User", password: "smith1234", password_confirmation: "smith1234"])
+
+# Checkout.destroy_all
+# checkout1 =

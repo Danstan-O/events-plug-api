040023b11e1953b832486bf7d9c33ade0a4c46a1
danstan <danstan.oduor@student.moringaschool.com>
2023-01-13 16:01:07 +0300

    edited the event_details and users cotroller

--- app/serializers/event_details_serializer.rb
+++ app/serializers/event_details_serializer.rb
@@ -1,3 +1,5 @@
 class EventDetailsSerializer < ActiveModel::Serializer
-     attributes :name, :address, :location, :category, :description, :like, :price, :image, :start_date, :end_date, :start_time, :end_time, :users
+     attributes :name, :address, :location, :category, :description, :like, :price, :image, :start_date, :end_date, :start_time, :end_time
+     attributes :users
+     
   end

726448ecffc9a0cbfb5a9baa9a8a52ed1084ef47
danstan <danstan.oduor@student.moringaschool.com>
2023-01-17 10:08:13 +0300

    added validations to account_infos model

--- app/models/account_info.rb
+++ app/models/account_info.rb
@@ -2,7 +2,7 @@ class AccountInfo < ApplicationRecord
   belongs_to :user
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :billing_address, presence: true
-  # validates :email, presence: true, uniqueness: true
+  # validates :billing_address, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :phone_number, presence: true, length: { minimum: 10 }
 end

325beda2f38a21c5d97c60a364a6beb5b28ff7c1
danstan <danstan.oduor@student.moringaschool.com>
2023-01-17 11:46:23 +0300

    added a create method to help connect tables

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,2 +1,11 @@
 class UserEventsController < ApplicationController
+    def create
+        account_infos =  UserEvent.create!(user_events_params)
+        render json: account_infos, status: :created
+    end
+
+    private
+    def user_events_params
+        params.permit(:user_id, :event_id)
+    end
 end

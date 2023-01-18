a21ed75aa2cd1136a9e89b44afa0a88e5cff106e
danstan <danstan.oduor@student.moringaschool.com>
2023-01-18 13:14:56 +0300

    updated relationships and added more validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,10 @@ class Event < ApplicationRecord
 
   has_many :user_events, dependent: :destroy
   has_many :users, through: :user_events  
+  belongs_to :user
   validates :name, presence: true  
+  validates :price, presence: true 
   validates :address, presence: true  
   validates :description, presence: true, length: { minimum: 25 }
-  validates :category, presence: true, inclusion: { in: ["Hiking and Adventure", "Music and Concert", "Food and Drinks", "GEneral events", "Sports and Entertainment", "IT and Technology"] }
+  validates :category, presence: true, inclusion: { in: ["Hiking and Adventure", "Music and Concert", "Food and Drinks", "GEneral events", "Sports and Entertainment", "IT and Technology", "Tech hangout venues"] }
 end

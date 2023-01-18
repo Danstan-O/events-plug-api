9e939c1d39e098c5c57d6f8f2891a5d710f2186d
danstan <danstan.oduor@student.moringaschool.com>
2023-01-18 17:30:04 +0300

    uncommented the user_events controller

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,15 +1,15 @@
-# class UserEventsController < ApplicationController
+class UserEventsController < ApplicationController
 
     
-#     def create
-#         user_events =  UserEvent.create!(user_events_params)
-#         render json: user_events, status: :created
-#     end
+    def create
+        user_events =  UserEvent.create!(user_events_params)
+        render json: user_events, status: :created
+    end
 
 
 
-#     private
-#     def user_events_params
-#         params.permit(:user_id, :event_id)
-#     end
-# end
+    private
+    def user_events_params
+        params.permit(:user_id, :event_id)
+    end
+end
